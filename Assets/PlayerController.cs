b216ec2118222005ea3898f0540cca8a59cca41e
tzutzu858 <chenyingtzu858@gmail.com>
2020-04-09 21:20:16 +0800

    4_9

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 跳躍
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // 遊戲角色的速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // 速度限制
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // (追加)依照行進方向翻轉
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+    }
+}
